72f7599e2583873e3ec1041a06bf53627fcb5453
BrunoCardos <brunocardoso@sapo.cv>
2023-06-25 22:38:59 -0100

    Add the What GPT3 Section elements

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibility are beyond your imagination</h1>
+        <p>Explore the library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
