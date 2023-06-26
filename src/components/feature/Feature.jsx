199025d46302fe088df33d0c6826ea9b7fee9245
BrunoCardos <brunocardoso@sapo.cv>
2023-06-25 23:15:21 -0100

    Add all the items of Feature Component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container-feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container-feature-text'>
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
 
